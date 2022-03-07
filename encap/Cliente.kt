87c8d6ba5712e2868ce079926225d2aba5676202
Kaioleite <kaioleite1998@gmail.com>
2022-03-07 19:33:30 -0300

    Exercicio Class advanced

--- encap/Cliente.kt
+++ encap/Cliente.kt
@@ -4,60 +4,61 @@ package encap
     var end = ""
     var telefone = ""
     private val listaDeCompras = mutableListOf<String>()
-    var produto = ""
-
 
     constructor(
         nome:String,
         end:String,
-    telefone:String
     ):this(nome)
     {
         this.end = end
-        this.telefone = telefone
+
     }
     constructor(
         nome:String,
         end:String,
-        telefone:String,
-        produto: String
-             ):this(nome, end, telefone)
+        telefone:String
+             ):this(nome, end)
     {
-                this.produto = produto
+         this.telefone = telefone
                 }
-
-
-    fun CadItem(produto: String){
+     init{
+         if(nome.isEmpty())
+             throw Exception("A classe cliente não foiinstanciada corretamente.")
+     }
+    fun addProd(produto:String){
 
         if(produto.isEmpty()){
             println("Produto Não exitente!!!!!")
         }else{
           listaDeCompras.add(produto)
+            println("O item $produto foi Adicionado com Sucesso!!")
         }
     }
 
     fun removeP(produto: String){
         if(produto.isEmpty()){
             println("Produto inválido")
-        }else if(produto.contains(produto)){
-            listaDeCompras.add(produto)
-            println("Produto removido com sucesso")
+
+        }else if(listaDeCompras.contains(produto)){
+            listaDeCompras.remove(produto)
+            println("O Item $produto removido com sucesso")
+
         }else{
             println("Esse Produto não existe na lista")
         }
     }
-    fun ListProd(produto: String) {
-        produto.forEach {
-            println(it)
+    fun ListaProduto(){
+       println(" \n *** Compras do Cliente $nome *** \n")
+        listaDeCompras.forEach{
+            print("\n $it")
+        }
 
         }
-    }
 
-override fun toString(): String {
-    return "Nome do Cleinte - $nome" +
+      override fun toString(): String {
+       return "Nome do Cleinte - $nome" +
             "\nEndereço  - $end" +
             "\ntelefone $telefone " +
-            "\nLista  $listaDeCompras "+
-            "\nProduto - $produto "
+            "\nLista  $listaDeCompras "
 }
 }
